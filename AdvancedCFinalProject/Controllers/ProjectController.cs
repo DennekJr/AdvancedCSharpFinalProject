57c45e2f48dc4ed5e7d96f10a0a7397beffa134e
Adeola Adetoyese <adeolakautharadetoyese@mitt.ca>
2022-04-05 15:19:41 -0500

    Create Project Controller

--- AdvancedCFinalProject/Controllers/ProjectController.cs
+++ AdvancedCFinalProject/Controllers/ProjectController.cs
@@ -1,6 +1,7 @@
 ﻿using AdvancedCFinalProject.Data;
 using AdvancedCFinalProject.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace AdvancedCFinalProject.Controllers
 {
@@ -29,6 +30,75 @@ namespace AdvancedCFinalProject.Controllers
                 return RedirectToAction(nameof(Index));
             }
             return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> UpdateProject(int? id)
+        {
+            {
+                ViewBag.Id = id;
+                if (id == null)
+                {
+                    return NotFound();
+                }
+
+                var project = await db.Project.FindAsync(id);
+                if (project == null)
+                {
+                    return NotFound();
+                }
+                return View();
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> UpdateProject(int id, [Bind("ProjectId,Title,IsComplete,Tasks")] Project project)
+        {
+            if (id != project.ProjectId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    db.Update(project);
+                    await db.SaveChangesAsync();
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex.Message);
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var project = await db.Project.FindAsync(id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                db.Project.Remove(project);
+                await db.SaveChangesAsync();
+            }
+            return View(nameof(Index));
+        }
+
+        public IActionResult Details(int? id)
+        {
+            Project project = db.Project.Find(id);
+            return View(project);
         }
 
         public IActionResult Index()
