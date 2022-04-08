d9c5184bad5ac7c6a0c5ad8d3e84b4a2db27e3a3
DennekJr <54910517+DennekJr@users.noreply.github.com>
2022-04-08 00:45:24 -0500

    create project fix

--- AdvancedCFinalProject/Controllers/ProjectController.cs
+++ AdvancedCFinalProject/Controllers/ProjectController.cs
@@ -2,6 +2,7 @@
 using AdvancedCFinalProject.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace AdvancedCFinalProject.Controllers
 {
@@ -15,20 +16,29 @@ namespace AdvancedCFinalProject.Controllers
             db = context;
         }
 
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = db.Project;
+            return View(await applicationDbContext.ToListAsync());
+        }
+
         public IActionResult CreateProject(int? Cid)
         {
+            ViewBag.YourEnums = new SelectList(Enum.GetValues(typeof(Priority)), Priority.None);
             var comp = db.Company.FirstOrDefault(c => c.CompanyId == Cid);
             ViewBag.Company = comp.CompanyId;
             return View();
         }
 
         [HttpPost]
-        public async Task<IActionResult> CreateProject(int? Cid, [Bind("ProjectId,Title")] Project project)
+        public async Task<IActionResult> CreateProject(int? Cid, [Bind("ProjectId,Title,Content,IsComplete,Priority")] Project project)
         {
             var comp = db.Company.FirstOrDefault(c => c.CompanyId == Cid);
-            ViewBag.Company = comp;
+            ViewBag.Company = comp.CompanyId;
+            ViewBag.YourEnums = new SelectList(Enum.GetValues(typeof(Priority)), Priority.None);
             if (ModelState.IsValid)
             {
+                project.CompanyId = comp.CompanyId;
                 comp.Projects.Add(project);
                 db.Project.Add(project);
                 await db.SaveChangesAsync();
@@ -104,12 +114,6 @@ namespace AdvancedCFinalProject.Controllers
         {
             Project project = db.Project.Find(id);
             return View(project);
-        }
-
-        public IActionResult Index()
-        {
-            List<Project> allProjects = db.Project.ToList();
-            return View(allProjects);
         }
     }
 }
