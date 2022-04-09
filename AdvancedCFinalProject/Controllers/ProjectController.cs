977fb7d80508c268b6f8bea6eb1b11f4e2da9e85
DennekJr <54910517+DennekJr@users.noreply.github.com>
2022-04-08 19:25:44 -0500

    Update ProjectController.cs

--- AdvancedCFinalProject/Controllers/ProjectController.cs
+++ AdvancedCFinalProject/Controllers/ProjectController.cs
@@ -119,6 +119,7 @@ namespace AdvancedCFinalProject.Controllers
                     return NotFound();
                 }
                 return View();
+                
             }
         }
 
